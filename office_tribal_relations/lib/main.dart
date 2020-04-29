a9aface9225c9c37e0c1bd7f020d826eb90d285f
Jason Flaherty <jflaherty@fs.fed.us>
2020-04-29 15:16:17 -0700

    homepage text update

--- office_tribal_relations/lib/main.dart
+++ office_tribal_relations/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:office_tribal_relations/widgets/categoryList.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:flutter_html/flutter_html.dart';
 
 void main() => runApp(new OTRApp());
 
@@ -63,7 +64,8 @@ class _OtrHomePageState extends State<OtrHomePage> {
 
 //split out the overlay text
 Widget _HomepageWords(BuildContext context) {
-  return Column(
+  return SingleChildScrollView(
+    child: Column(
     mainAxisAlignment: MainAxisAlignment.start,
     crossAxisAlignment: CrossAxisAlignment.start,
     children: <Widget>[
@@ -109,6 +111,21 @@ Widget _HomepageWords(BuildContext context) {
                     fontWeight: FontWeight.bold,
                     color: Colors.white),
               ),
+              Container(
+                margin: EdgeInsets.all(15),
+              ),
+              Html(
+                data:"""<h1>About the Office of  Tribal Relations</h1>
+<p>The U.S. Forest Service  established the first Tribal Government Program Manager position in the  Washington Office in 1988, responding to identified needs and Executive  direction. Subsequently, in 2004, the <strong>Office of Tribal Relations </strong>was  formed as a permanent staff within the State and Private Forestry Deputy Area,  to facilitate consistency and effectiveness in Forest Service program delivery  to Tribes, and to institutionalize long-term consultative and collaborative  relationships with tribal governments through new policy and direction. The  current Office of Tribal Relations staff consists of six employees who serve as  the Headquarters component of the Forest Service's Tribal Relations Program.  Field staffs comprise the other part of the program, and include the Regional  Program Managers, Tribal Liaisons at the Forest level, and individuals in each  of the Agency's mission areas.</p>
+<p><strong>The Office of Tribal  Relations:</strong></p>
+<ul>
+  <li>Provides oversight of Forest Service programs and policy       that may affect Tribes, encouraging and supporting respectful, supportive       government-to-government relationships that strengthen external and       internal coordination and communication about tribal concerns and the       Forest Service mission.</li>
+  <li>Prepares and implements new and existing policy and       direction outlining the legal requirements and opportunities within       existing authorities relating to Tribes.</li>
+  <li>Clarifies the Agency's responsibilities regarding       Tribal trust and reserved rights.</li>
+  <li>Develops and supports education and training for       employees of the Forest Service and other agencies, helping them work more       effectively with tribal governments and other partners.</li>
+  <li>Explores innovative ways to interact with Tribes,       Tribal Members, and others to enhance the Forest Service's service to       Native American communities.</li>
+</ul>
+<p>The Office of Tribal  Relations supports meaningful and significant collaboration and consultation  with Tribes across all program areas. The Office of Tribal Relations is  committed to help increase opportunities for Tribes to benefit from the Forest  Service programs and to help the Forest Service benefit from input from Tribes,  in support of Tribal Sovereignty, self-governance, and self-determination, as  well as Forest Service goals such as adaptation and mitigation of climate  change. The Office of Tribal Relations is initializing and institutionalizing  relationships with internal and external partners, working closely with other  staffs to ensure Tribal concerns and opportunities are addressed in new  policies, and developing implementation processes for new authorities.</p><p><strong>Find Out More >></strong></p>""",defaultTextStyle: TextStyle(color: Colors.white),),
             ],
           ),
         ),
@@ -120,7 +137,7 @@ Widget _HomepageWords(BuildContext context) {
         },
       ),
     ],
-  );
+  ));
 }
 
 //fade in image
